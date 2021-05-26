8792561778065af7bf0e4966c4825d63ddfd2974
Anderson Misson <missondeveloper@gmail.com>
2021-05-25 22:22:33 -0300

    Capitulo 02 - Exemplo 02-01

---
+++ capitulo02/exemplo_02_01/fig02_01.c
@@ -0,0 +1,11 @@
+/* Figura 2.1: fig02_01.c
+   Primeiro Programa em C */
+#include <stdio.h>
+
+/* Função main indica execução do programa */
+int main( void ){
+
+	printf("Bem vindo ao C!\n");
+
+	return 0; /* Indica que o programa terminou com sucesso */
+} /* fim da função main */
