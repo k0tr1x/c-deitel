db1c7ab1a5f99df759aa4d1030a33c5c9729eef8
Anderson Misson <missondeveloper@gmail.com>
2021-05-27 17:18:15 -0300

    Capitulo 02 - Exemplo 02-04

---
+++ capitulo02/exemplo_02_04/fig02_04.c
@@ -0,0 +1,11 @@
+/* Figura 2.1: fig02_04.c
+   Imprimindo várias linhas com uma única instrução printf */
+#include <stdio.h>
+
+/* Função main indica execução do programa */
+int main( void ){
+
+	printf("Bem vindo\nao\nC!\n");
+
+	return 0; /* Indica que o programa terminou com sucesso */
+} /* fim da função main */
